50014d050897f393957043a3e0b5aae95071bddd
Fabian DC Beier <fabiandcbeier@Fabians-iMac.local>
2020-11-17 11:49:42 +0100

    first styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="flex justify-center">
     <Nuxt />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container">
-    <li v-for="post of posts" :key="post.slug">
+  <div class="container flex max-w-screen-lg">
+    <li v-for="post of posts" :key="post.slug" class="text-2xl text-white list-none shadow-lg m-6 p-4 bg-pink-400 duration-200 hover:bg-pink-800 w-1/3">
       <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
-      <p>{{post.description}}</p>
+      <p class="text-base">{{post.description}}</p>
     </li>
   </div>
 </template>
@@ -10,7 +10,7 @@
 <script>
 export default {
   async asyncData({ $content }) {
-    const posts = await $content("recipes").fetch();
+    const posts = await $content("recipes").sortBy('title').fetch();
 
     return {
       posts,
