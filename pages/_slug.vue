bbc6ae5dc8392a615f8d4b1ed775e02b972c1f9b
fabianbeier <fabianbeier@online.de>
2020-11-17 08:26:04 +0100

    Article-Test

---
+++ pages/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    <h2>{{ post.title }}</h2>
+    <h2 class="bg-teal-300 ">{{ post.description }}</h2>
+    <nuxt-content :document="post" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    let post;
+    try {
+      post = await $content("blog", params.slug).fetch();
+      // OR const article = await $content(`articles/${params.slug}`).fetch()
+    } catch (e) {
+      error({ message: "Blog Post not found" });
+    }
+
+    return {
+      post,
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
